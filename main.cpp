c6464772a1e0548c19b111f6dce9fd76c51d952b
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-04-05 17:41:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int n1 , n2 ;
+ int n3 ;
+ n1=0;
+ n2=1;
+ cout<<n1<<"";
+ cout<<n2<<"";
+ for( int i=1 ; i>=15 ; i++ ) {
+     n3=n2+n1;
+     cout<<n3<<"";
+     n1=n2;
+     n2=n3;
+
+    }}
+   
    return 0;
 }
